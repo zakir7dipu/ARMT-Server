7b6d92a08a9428b4c94aaf6ac13111462b7699f5
Anwar Ullah <anwar.ullah@ssgbd.com>
2022-03-15 15:26:45 +0600

    events seed, events file upload, events database updated_

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -15,6 +15,33 @@ class EventSeeder extends Seeder
     {
         $events = [
             [
+                'image' => '/upload/events/The-Quest-to-Provide-Safe-Drinking-Water-for-All_320X165.jpg',
+                'date' => '2020-04-01',
+                'start_at' => '09:00:00',
+                'end_at' => '10:00:00',
+                'location' => 'Bangladesh',
+                'title' => 'The Quest to Provide Safe Drinking Water for All - Water Project!',
+                'description' => "<p>The world may have marched into the 21st century with plenty of economic, cultural, and technological advancements, but there continue to be areas of the planet which still lack the most basic of resources or amenities. There are still entire regions that lack a supply of one of the most vital elements required for life to survive — water. </p>
+
+                <p>While the average human being needs several gallons of water per day to lead what is considered a normal life, it is important for at least 2-3 liters of that to be clean, safe, adulterant-free drinkable water. If a person does not have access to this, they run the risk of dehydration, electrolyte imbalance, and developing kidney problems such as stones or outright failure. At the same time, they may try to quench their thirst using unclean or unsafe water, which may lead to deadly diseases such as cholera, diarrhea, and dysentery.</p>
+
+                <p>However, this can be extremely difficult to get in remote areas of underdeveloped or developing countries, and Bangladesh is one such country. Roughly 10% of the nearly 170-million-strong population do not have a source of clean water available to them.</p> 
+
+                <p>The plight of this group of people — who can be found in almost all parts of the country — is as extreme as it is unfortunate, with some people having to walk many kilometers to and from a source of clean water, with the weight of the water adding to the difficulty of the task on the way back. </p>
+
+                <p>Alternatively, other families have to pay exorbitant amounts to exploitative groups which only allow these marginalized people to use their deep-bore tube wells and pumps for a price, leveraging their need for this basic resource to make money. In many cases, some of these families are forced to pay such sums despite making a paltry living as they do not have anyone able enough to make a regular journey to acquire water.</p>
+
+                <p>In such dire circumstances, the Abdur Rahim Memorial Trust has come forward to stand by such groups. Using their own resources, the Trust has set up pumps, tube wells, pipes, and tanks for anyone and everyone to use. In order to maintain absolute neutrality, the Trust had most of these watering stations built inside the perimeters of mosques, where they are beyond the reach of the ill-intent of any influential locals. Along with extensive plumbing facilities (some of which include tube wells as deep as 280 feet), the organization has also built oju khanas — separate areas for Muslims to cleanse themselves before prayer — for some of the mosques.</p> ",
+            ],[
+                'image' => '/upload/events/SSG-for-Humanity.jpg',
+                'date' => '2020-04-01',
+                'start_at' => '09:00:00',
+                'end_at' => '10:00:00',
+                'location' => 'Bangladesh',
+                'title' => 'SSG for Humanity',
+                'description' => "<p>During the pandemic, people from every class suffered a lot. Especially the poor underprivileged people suffered from the food crisis. In this dire situation, Super Star Group the patron of ARMT decided to come forward and share the same food that they provide for their regular employees. This initiative still continues and every month from SSG a great amount of food is distributed to the poor underprivileged people of the society.</p> 
+                <p>Super Star Group the patron of ARMT always works for the wellbeing of underprivileged people of the society. And in the future, it has a plan to make this happen on a very large scale. </p> ",
+            ],[
                 'image' => '/upload/events/A-Haven-of-Health-for-Those-in Need_320X165.jpg',
                 'date' => '2020-04-01',
                 'start_at' => '09:00:00',
@@ -58,26 +85,7 @@ class EventSeeder extends Seeder
 
                 <p>Of the 1 Crore 15 Lakh estimated budget, roughly Tk 6,775,000 has been raised, with another TK 4,725,000 required. Should this requirement be met, the lives of not only the people of Patdal, but also those living in villages nearby will be transformed.</p>
                 ",
-            ],
-            [
-                'image' => '/upload/events/The-Quest-to-Provide-Safe-Drinking-Water-for-All_320X165.jpg',
-                'date' => '2020-04-01',
-                'start_at' => '09:00:00',
-                'end_at' => '10:00:00',
-                'location' => 'Bangladesh',
-                'title' => 'The Quest to Provide Safe Drinking Water for All',
-                'description' => "<p>The world may have marched into the 21st century with plenty of economic, cultural, and technological advancements, but there continue to be areas of the planet which still lack the most basic of resources or amenities. There are still entire regions that lack a supply of one of the most vital elements required for life to survive — water. </p>
-
-                <p>While the average human being needs several gallons of water per day to lead what is considered a normal life, it is important for at least 2-3 liters of that to be clean, safe, adulterant-free drinkable water. If a person does not have access to this, they run the risk of dehydration, electrolyte imbalance, and developing kidney problems such as stones or outright failure. At the same time, they may try to quench their thirst using unclean or unsafe water, which may lead to deadly diseases such as cholera, diarrhea, and dysentery.</p>
-
-                <p>However, this can be extremely difficult to get in remote areas of underdeveloped or developing countries, and Bangladesh is one such country. Roughly 10% of the nearly 170-million-strong population do not have a source of clean water available to them.</p> 
-
-                <p>The plight of this group of people — who can be found in almost all parts of the country — is as extreme as it is unfortunate, with some people having to walk many kilometers to and from a source of clean water, with the weight of the water adding to the difficulty of the task on the way back. </p>
-
-                <p>Alternatively, other families have to pay exorbitant amounts to exploitative groups which only allow these marginalized people to use their deep-bore tube wells and pumps for a price, leveraging their need for this basic resource to make money. In many cases, some of these families are forced to pay such sums despite making a paltry living as they do not have anyone able enough to make a regular journey to acquire water.</p>
-
-                <p>In such dire circumstances, the Abdur Rahim Memorial Trust has come forward to stand by such groups. Using their own resources, the Trust has set up pumps, tube wells, pipes, and tanks for anyone and everyone to use. In order to maintain absolute neutrality, the Trust had most of these watering stations built inside the perimeters of mosques, where they are beyond the reach of the ill-intent of any influential locals. Along with extensive plumbing facilities (some of which include tube wells as deep as 280 feet), the organization has also built oju khanas — separate areas for Muslims to cleanse themselves before prayer — for some of the mosques.</p> ",
-            ],
+            ]
         ];
 
         \App\Models\Event::truncate();
